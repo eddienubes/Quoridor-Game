264ca9380777bd39e1e3705975d3e3f5070aa041
Maks <maksterekhov1704@gmail.com>
2021-10-05 14:42:32 +0300

    Изменения цвета через шейдер

--- CoridorGame/Assets/Scripts/View/Cell.cs
+++ CoridorGame/Assets/Scripts/View/Cell.cs
@@ -1,4 +1,5 @@
 using System;
+using Codice.Client.BaseCommands;
 using UnityEngine;
 
 
@@ -9,33 +10,56 @@ namespace View
         public Vector3 Size { get; private set; }
         public Transform Transform { get; private set; }
 
-        public Action OnClicked;
+        public event Action OnClicked;
+
+        private Material _material;
+
+        private static readonly int IsSelected = Shader.PropertyToID("_isSelected");
+        private static readonly int IsHighlighted = Shader.PropertyToID("_isHighlighted");
+
+
+        #region UnityEvents
+
         private void OnMouseEnter()
         {
-            GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+            SetHighlighted(true);
         }
 
         private void OnMouseExit()
         {
-            GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            SetHighlighted(false);
         }
 
         private void OnMouseDown()
         {
-            GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+            SetHighlighted(true);
             OnClicked?.Invoke();
         }
 
         private void Awake()
         {
             Transform = transform;
+
             var transformSize = Transform.localScale;
+            _material = GetComponent<Renderer>().material;
             var colliderSize = GetComponent<BoxCollider>().size;
             Size = new Vector3(
                 colliderSize.x * transformSize.x,
                 colliderSize.y * transformSize.y,
                 colliderSize.z * transformSize.z
             );
+        }
+
+        #endregion
+
+        public void SetSelected(bool isSelected)
+        {
+            _material.SetFloat(IsSelected, isSelected ? 1 : 0);
+        }
+
+        public void SetHighlighted(bool isHighlighted)
+        {
+            _material.SetFloat(IsHighlighted, isHighlighted ? 1 : 0);
         }
     }
 }
