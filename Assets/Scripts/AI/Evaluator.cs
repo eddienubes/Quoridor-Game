add77b7c9ea7ba0339f525329e9431b7e42691ed
SunCreamGames <tg : @Sun_Cream>
2021-11-04 17:03:02 +0200

    Upgrading sev

--- Assets/Scripts/AI/Evaluator.cs
+++ Assets/Scripts/AI/Evaluator.cs
@@ -1,16 +1,39 @@
 namespace Quorridor.AI
 {
+    using System.Collections.Generic;
     using Model;
 
     public static class Evaluator
     {
         public static int HeuristicCost(Player player, Game game, Grid grid)
         {
-            var pawn = player.Pawn;
-            var currentCell = grid.GetPawnCell(pawn);
-            var shortestPath = grid.GetShortestPath(currentCell, pawn.WinLineY);
+            var result = 0;
+            int shortestPathPlayer = 0;
 
-            return shortestPath;
+            var opponentsShortestPaths = new List<int>();
+
+            foreach (var gamePlayer in game.Players)
+            {
+                var pawn = gamePlayer.Pawn;
+                var currentCell = grid.GetPawnCell(pawn);
+
+                if (gamePlayer == player)
+                {
+                    result += gamePlayer.WallsCount * 2;
+                    shortestPathPlayer = grid.GetShortestPath(currentCell, pawn.WinLineY);
+                    continue;
+                }
+
+                result -= gamePlayer.WallsCount * 2;
+                opponentsShortestPaths.Add(grid.GetShortestPath(currentCell, pawn.WinLineY));
+            }
+
+            foreach (var pathLenght in opponentsShortestPaths)
+            {
+                result += (pathLenght - shortestPathPlayer) * 5;
+            }
+
+            return result;
         }
     }
 }
