a30ef8146329b3095dd2745bb071037071a8b1b8
Maks <maksterekhov1704@gmail.com>
2021-11-05 01:39:01 +0200

    Замена проекта на консольный

---
+++ QuorridorAI/Model/Game.cs
@@ -0,0 +1,124 @@
+namespace Quorridor.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Commands;
+    using Grid = Grid;
+
+    public class Game
+    {
+        private Grid _grid;
+
+        private Player[] _players;
+
+        private Stack<IMakeTurnCommand> _gameLog;
+
+        public event Action<Player> OnGameEnded;
+        public event Action<Player> OnTurnSwitched;
+        public event Action<Player, (int, int), (int, int)> OnPlayerMoved;
+
+        public Player[] Players => _players;
+
+        public Game(Grid grid, params Player[] players)
+        {
+            _grid = grid;
+            _players = players;
+            _gameLog = new Stack<IMakeTurnCommand>();
+        }
+
+
+        public void PlacingWall(Player player, bool isVertical, (int, int) cell1Pair1, (int, int) cell2Pair1,
+            (int, int) cell1Pair2, (int, int) cell2Pair2)
+        {
+            if (!player.IsActiveTurn)
+            {
+                throw new Exception("Player isn't active");
+            }
+
+            if (player.WallsCount <= 0)
+            {
+                throw new Exception("Walldeck is empty");
+            }
+
+            IMakeTurnCommand turnCommand =
+                new PlaceWallCommand(_grid, cell1Pair1, cell2Pair1, cell1Pair2, cell2Pair2, isVertical);
+            turnCommand.Execute();
+
+            if (!_grid.CheckPaths(_players))
+            {
+                turnCommand.Undo();
+                throw new Exception("Wall can be placed because it will close last path to win for one of the players");
+            }
+
+            _gameLog.Push(turnCommand);
+
+            player.OnWallPlacedInvoke(isVertical, cell1Pair1, cell2Pair1, cell1Pair2, cell2Pair2);
+            SwitchTurn(player);
+        }
+
+        public void MovingPlayer(Pawn playerPawn, int startX, int startY, int targetX, int targetY)
+        {
+            var player = _players.FirstOrDefault(p => p.Pawn == playerPawn);
+            if (player == null)
+            {
+                throw new Exception($"There is no player for this pawn. Id: {playerPawn.PlayerId}");
+            }
+
+            if (!player.IsActiveTurn)
+            {
+                throw new Exception($"This player is inactive. Id: {playerPawn.PlayerId}");
+            }
+
+            IMakeTurnCommand turnCommand = new MovePawnCommand(player.Pawn, _grid, startX, startY, targetX, targetY);
+
+            turnCommand.Execute();
+            _gameLog.Push(turnCommand);
+            CheckGameForFinishing(player);
+
+            OnPlayerMoved?.Invoke(player, (startX, startY), (targetX, targetY));
+
+            SwitchTurn(player);
+        }
+
+        public void MovingPlayer(Pawn playerPawn, Cell start, Cell target)
+        {
+            var player = _players.FirstOrDefault(p => p.Pawn == playerPawn);
+            if (player == null)
+            {
+                throw new Exception($"There is no player for this pawn. Id: {playerPawn.PlayerId}");
+            }
+
+            if (!player.IsActiveTurn)
+            {
+                throw new Exception($"This player is inactive. Id: {playerPawn.PlayerId}");
+            }
+
+            IMakeTurnCommand turnCommand = new MovePawnCommand(player.Pawn, _grid, start, target);
+
+            turnCommand.Execute();
+            _gameLog.Push(turnCommand);
+            CheckGameForFinishing(player);
+
+            OnPlayerMoved?.Invoke(player, (start.GridX, start.GridY), (target.GridX, target.GridY));
+
+            SwitchTurn(player);
+        }
+        
+        private void SwitchTurn(Player player)
+        {
+            var nextPlayerIndex = (Array.IndexOf(_players, player) + 1) % _players.Length;
+            player.EndTurn();
+            OnTurnSwitched?.Invoke(player);
+            _players[nextPlayerIndex].StartTurn();
+        }
+
+        private void CheckGameForFinishing(Player p)
+        {
+            if (_grid.CheckIsPawnOnTheWinLine(p.Pawn))
+            {
+                OnGameEnded?.Invoke(p);
+            }
+        }
+    }
+}
---
+++ QuorridorAI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Quorridor.AI;
+using Quorridor.Model;
+
+namespace QuorridorAI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            InitGame(true);
+        }
+
+        public static void InitGame(bool isPlayerFirstTurn)
+        {
+            const int WALLS_COUNT = 10;
+            const int FIELD_SIZE_Y = 9;
+            var gameController = new CliGameController();
+
+            var players = new Player[]
+            {
+                new HotSeatPlayer(GetWinLine(true), !isPlayerFirstTurn, GetPlayerId(true), WALLS_COUNT),
+                new DummyAiPlayer(GetWinLine(false), isPlayerFirstTurn, GetPlayerId(false), WALLS_COUNT),
+            };
+
+            var playerControllers = new IPlayerController[]
+            {
+                new CLIPlayerController(),
+                new CLIPlayerController(),
+            };
+
+            if (!isPlayerFirstTurn)
+            {
+                players = players.Reverse().ToArray();
+                playerControllers = playerControllers.Reverse().ToArray();
+            }
+            
+            gameController.Init(playerControllers, players);
+            players.First().StartTurn();
+            
+            int GetPlayerId(bool isPlayer) => isPlayerFirstTurn == isPlayer ? 0 : 1;
+            int GetWinLine(bool isPlayer) => isPlayerFirstTurn == isPlayer ? FIELD_SIZE_Y : 0;
+        }
+    }
+}
